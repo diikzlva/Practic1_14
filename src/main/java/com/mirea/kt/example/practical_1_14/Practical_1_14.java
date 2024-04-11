6a8dbb85550b0e63edd16bf789976a59a9a270ea
Динара <@LAPTOP-EV6I0PGS>
2024-04-12 00:51:19 +0300

    5 commit

--- src/main/java/com/mirea/kt/example/practical_1_14/Practical_1_14.java
+++ src/main/java/com/mirea/kt/example/practical_1_14/Practical_1_14.java
@@ -9,10 +9,8 @@ public class Practical_1_14 {
     public static void main(String[] args) {
         System.out.println ("Козлова Динара Артёмовна, РИБО-01-22, Вариант 2, ");
 
-        // Создание одного семафора для управления доступом к столику
         Semaphore semaphore = new Semaphore(2);
 
-        // Создание и запуск потоков студентов
         for (int i = 1; i <= 7; i++) {
             Student student = new Student(i, semaphore);
             Thread studentThread = new Thread(student);
--- src/main/java/com/mirea/kt/example/practical_1_14/Student.java
+++ src/main/java/com/mirea/kt/example/practical_1_14/Student.java
@@ -16,15 +16,12 @@ public class Student implements Runnable {
     @Override
     public void run() {
         try {
-            // Студент ожидает свободного места за столом
             System.out.println("Student" + id + " waiting");
             semaphore.acquire();
 
-            // Студент начинает есть
             System.out.println("Student" + id + " eating");
-            Thread.sleep(3000); // Задержка на 3 секунды для имитации обеда
+            Thread.sleep(3000); 
 
-            // Студент выходит из-за стола
             System.out.println("Student" + id + " exit");
             semaphore.release();
         } catch (InterruptedException ex) {
